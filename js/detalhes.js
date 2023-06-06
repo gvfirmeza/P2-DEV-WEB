85bd37ff77bc25823c43fa21cbb3b203a3d6b5de
gvfirmeza <pmvieira@outlook.com>
2023-06-06 19:03:57 -0300

    acho que terminei

--- js/detalhes.js
+++ js/detalhes.js
@@ -1,8 +1,8 @@
 
-    document.getElementById('nome').textContent = localStorage.getItem('nome');
-    document.getElementById('nome_completo').textContent = localStorage.getItem('nome_completo');
-    document.getElementById('posicao').textContent = localStorage.getItem('posicao');
+    document.getElementById('nome').textContent = localStorage.getItem('nome').toUpperCase();
+    document.getElementById('nome_completo').textContent = "Nome Completo: " + localStorage.getItem('nome_completo');
+    document.getElementById('posicao').textContent = localStorage.getItem('posicao').toUpperCase();
     document.getElementById('descricao').textContent = localStorage.getItem('descricao');
-    document.getElementById('nascimento').textContent = localStorage.getItem('nascimento');
-    document.getElementById('altura').textContent = localStorage.getItem('altura');
+    document.getElementById('nascimento').textContent = "Nascimento: " + localStorage.getItem('nascimento');
+    document.getElementById('altura').textContent = "Altura: " + localStorage.getItem('altura');
     document.getElementById('imagem').src = localStorage.getItem('imagem');
